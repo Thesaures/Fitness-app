95acfe3c9f5cd59a9545d8b2f24a7cb652edd382
Ashik George <ashikgeorgec@gmail.com>
2024-08-27 09:35:47 +0530

    firebase

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,7 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
 
-  const { transformer, resolver } = config;
+module.exports = defaultConfig;
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
-  };
-
-  return config;
-})();
