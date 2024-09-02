e69abdb1bca8b5f7708193cdff4fd8d26ef0b4fe
Ashik George <ashikgeorgec@gmail.com>
2024-09-02 17:54:29 +0530

    added workout page

--- src/Navigations/UserNavigation.tsx
+++ src/Navigations/UserNavigation.tsx
@@ -4,6 +4,7 @@ import Intro from '../Screens/introduction';
 import BottomTabNavigation from './BottomNavigation';
 import Slider from '../Screens/slider';
 import Noification from '../Screens/notification';
+import Workout from '../Screens/Workout';
 const Stack = createNativeStackNavigator();
 
 const UserNavigation = () => {
@@ -33,6 +34,13 @@ const UserNavigation = () => {
        <Stack.Screen
         name="Noification"
         component={Noification}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Workout"
+        component={Workout}
         options={{
           headerShown: false,
         }}
--- src/constants/Constants.tsx
+++ src/constants/Constants.tsx
@@ -1,6 +1,79 @@
+import { equip, singleSet,exercises } from "../types/types";
+import One from '../../assets/images/imOne.svg';
+import Two from '../../assets/images/imTwo.svg';
+import Three from '../../assets/images/imThree.svg';
+import Four from '../../assets/images/imFive.svg';
+import Five from '../../assets/images/imFive.svg';
+import Six from '../../assets/images/imSix.svg'; 
+import Dumbell from '../../assets/images/dumbell.svg';
+import Bottle from '../../assets/images/bottle.svg';
+import Rope from '../../assets/images/rope.svg';
 export const SYMBOL_REGEX = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 export const CAPITAL_REGEX = /[A-Z]/;
 export const NUMBER_REGEX = /[0-9]/;
 export const USERNAME_REGEX = /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/;
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const CHECK_USERNAME = /^[A-Za-z]+ [A-Za-z]+$/
+export const CHECK_USERNAME = /^[A-Za-z]+ [A-Za-z]+$/;
+
+const set:singleSet[] = [
+    {
+       Svg:One,
+       name:'Warm Up',
+       set:'05:00'
+    },
+    {
+        Svg:Two,
+        name:'Jumping Jack',
+        set:'12x'
+     },
+     {
+        Svg:Three,
+        name:'Skipping',
+        set:'15x'
+     },
+     {
+        Svg:Four,
+        name:'Squats',
+        set:'20x'
+     },
+     {
+        Svg:Five,
+        name:'Arm Raises',
+        set:'00:53'
+     },
+     {
+        Svg:Six,
+        name:'Rest And Drink',
+        set:'02:00'
+     },
+
+]
+ 
+export const sets:exercises[]=[
+    {
+        id:'1',
+        set:set
+    },
+    {
+        id:'2',
+        set:set
+    },
+]
+export const data:equip[]=[
+    {
+        name:'Dumbell',
+        Svg:Dumbell
+    },
+    {
+        name:'Rope',
+        Svg:Rope
+    },
+    {
+        name:'Bottle',
+        Svg:Bottle
+    },
+    {
+        name:'Bottle',
+        Svg:Bottle
+    },
+]
